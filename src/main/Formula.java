39f41a1a1aa51cfb0603f4671a80d773950de7da
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 13:20:24 -0400

    bugs present

--- src/main/Formula.java
+++ src/main/Formula.java
@@ -1,13 +1,20 @@
 package main;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 public class Formula {
 
     //https://www.thebalance.com/loan-payment-calculations-315564
 
     //simple interest
     //principal x rate x time
+    //a = p(1 + rt)
+    //convert r to a decimal
 
+    //https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
     //compound interest
+    //amount = p(1+r/n)^(nt)
 
     //amortization
     //payment = amount / discount factor
@@ -15,13 +22,26 @@ public class Formula {
     //(p)eriodic interest rate: annual rate / # payments per year
     //discount factor: ([(1+p)^n]-1) / [p(1+p)^n]
 
-    public static double simpleInterest(){
-        double amount = 0.0;
+    //TODO: need something to deal with rounding
 
+    public static double percentToDecimal(double percent){
+        double rate = percent / 100;
+        return rate;
+    }
+
+    public static double monthsToYears(double months){
+        double years = months / 12;
+        return years;
+    }
+
+    public static double simpleInterest(double principal, double rate, double years){
+        double amount = 0.0;
+        double interest = (1 + (rate * years));
+        amount = principal * interest;
         return amount;
     }
 
-    public static double compundInterest(){
+    public static double compoundInterest(double principal, double rate, int months){
         double amount = 0.0;
 
         return amount;
