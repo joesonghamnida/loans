d322ea516b4bd59ad5f3ef9bc7635fb1775eaf47
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 12:21:47 -0400

    fleshing out

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,28 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    //current loan balance
+
+    //interest rate
+
+    //need to handle multiple loans and interest rates
+
+    //goal to pay it off
+
+    //budget
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Double> addLoan(){
+
+        //principal, interest rate
+        ArrayList<Double> loan = new ArrayList<>();
+
+        return loan;
+    }
 }
