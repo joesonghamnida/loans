1788f4615d94e50af7248b2480869017910431bb
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 12:22:47 -0400

    created outlines of tests

--- src/tests/FormulaTests.java
+++ src/tests/FormulaTests.java
@@ -1,6 +1,24 @@
 package tests;
 
+import main.Formula;
 import org.junit.*;
 
+import main.Formula.*;
+
 public class FormulaTests {
+
+    @Test
+    public void CalculateSimpleInterest(){
+        Formula.simpleInterest();
+    }
+
+    @Test
+    public void CalculateCompoundInterest(){
+        Formula.compundInterest();
+    }
+
+    @Test
+    public void CalculateAmortization(){
+        Formula.amortization();
+    }
 }
