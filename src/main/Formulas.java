c26b85eae0da113a988c8df2412defac8fe59ade
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 11:51:15 -0400

    shells

--- src/main/Formulas.java
+++ src/main/Formulas.java
@@ -1,4 +1,35 @@
 package main;
 
 public class Formulas {
+
+    //https://www.thebalance.com/loan-payment-calculations-315564
+
+    //simple interest
+    //principal x rate x time
+
+    //compound interest
+
+    //amortization
+    //payment = amount / discount factor
+    //n: payments per year x # of years
+    //(p)eriodic interest rate: annual rate / # payments per year
+    //discount factor: ([(1+p)^n]-1) / [p(1+p)^n]
+
+    public static double simpleInterest(){
+        double amount = 0.0;
+
+        return amount;
+    }
+
+    public static double compundInterest(){
+        double amount = 0.0;
+
+        return amount;
+    }
+
+    public static double amortization(){
+        double amount = 0.0;
+
+        return amount;
+    }
 }
