47ca4258cadf8e088c9a9282fa93f845c4d05094
Joe Morris <joe.morris.dev@gmail.com>
2017-07-18 18:01:53 -0500

    fleshing out compound interest

--- src/main/Formula.java
+++ src/main/Formula.java
@@ -6,33 +6,8 @@ import java.text.DecimalFormat;
 
 public class Formula {
 
-    //https://www.thebalance.com/loan-payment-calculations-315564
-
-    //simple interest
-    //principal x rate x time
-    //a = p(1 + rt)
-    //convert r to a decimal
-
-    //https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
-    //compound interest
-    //amount = p(1+r/n)^(nt)
-
-    //amortization
-    //payment = amount / discount factor
-    //n: payments per year x # of years
-    //(p)eriodic interest rate: annual rate / # payments per year
-    //discount factor: ([(1+p)^n]-1) / [p(1+p)^n]
-
     //https://docs.oracle.com/javase/8/docs/api/java/util/Currency.html
     //http://www.javapractices.com/topic/TopicAction.do?Id=13
-
-    //TODO: need something to deal with rounding
-    /* sample
-    public static double roundToDecimals(double d, int c)
-    {
-        int temp = (int)(d * Math.pow(10 , c));
-        return ((double)temp)/Math.pow(10 , c);
-    }*/
 
     public static BigDecimal percentToDecimal(double percent){
         BigDecimal rate = (BigDecimal.valueOf(percent / 100));
@@ -44,6 +19,12 @@ public class Formula {
         return years;
     }
 
+    //https://www.thebalance.com/loan-payment-calculations-315564
+
+    //simple interest
+    //principal x rate x time
+    //a = p(1 + rt)
+    //convert r to a decimal
     public static BigDecimal simpleInterest(BigDecimal principal, BigDecimal rate, double years){
         BigDecimal amount;
         BigDecimal interest = (rate.multiply(BigDecimal.valueOf(years)));
@@ -54,12 +35,27 @@ public class Formula {
         return amount;
     }
 
-    public static double compoundInterest(double principal, double rate, int months){
-        double amount = 0.0;
 
+    /*https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
+    compound interest
+    amount = p(1+r/n)^(nt)
+    p = principal
+    r = annual nominal interest rate as a decimal
+    n = number of compounding units per unit
+    t = time involved in years
+    */
+    public static BigDecimal compoundInterest(BigDecimal principal, BigDecimal rate, int compoundingUnits, double years){
+    	BigDecimal amount;
+
+    	amount = amount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
         return amount;
     }
 
+    //amortization
+    //payment = amount / discount factor
+    //n: payments per year x # of years
+    //(p)eriodic interest rate: annual rate / # payments per year
+    //discount factor: ([(1+p)^n]-1) / [p(1+p)^n]
     public static double amortization(){
         double amount = 0.0;
 
